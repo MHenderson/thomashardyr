a5fab746eb83dae333c4c5b662af8673c0d592cf
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-04-23 12:55:19 +0100

    Add documentation for all novel data sets.

---
+++ R/novels.R
@@ -0,0 +1,75 @@
+#' Thomas Hardy's Novels
+#' 
+#' This package contains the complete text of Thomas Hardy's novels.
+#' @docType package
+#' @name thomashardyr
+#' @aliases thomashardyr thomashardyr-package
+NULL
+
+#' The text of Thomas Hardy's novel "Far from the Madding Crowd"
+#'
+#' A dataset containing the text of Thomas Hardy's 1874 novel "Far from 
+#' the Madding Crowd". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/27}
+#' @format A character vector with 16293 elements
+"maddingcrowd"
+
+#' The text of Thomas Hardy's novel "The Return of the Native"
+#'
+#' A dataset containing the text of Thomas Hardy's 1878 novel "The Return of 
+#' the Native". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/122}
+#' @format A character vector with 15021 elements
+"native"
+
+#' The text of Thomas Hardy's novel "The Mayor of Casterbridge"
+#'
+#' A dataset containing the text of Thomas Hardy's 1886 novel "The Mayor of
+#' Casterbridge". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/143}
+#' @format A character vector with 12464 elements
+"mayor"
+
+#' The text of Thomas Hardy's novel "The Woodlanders"
+#'
+#' A dataset containing the text of Thomas Hardy's 1887 novel "The Woodlanders".
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each. 
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/482}
+#' @format A character vector with 14095 elements
+"woodlanders"
+
+#' The text of Thomas Hardy's novel "Tess of the D'Urbervilles"
+#'
+#' A dataset containing the text of Thomas Hardy's novel "Tess of the
+#' D'Urbervilles" published in 1891. The UTF-8 plain text was sourced from
+#' Project Gutenberg and is divided into elements of up to about 70 characters
+#' each. (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/110}
+#' @format A character vector with 16091 elements
+"tess"
+
+#' The text of Thomas Hardy's novel "Jude the Obscure"
+#'
+#' A dataset containing the text of Thomas Hardy's novel "Jude the Obscure", 
+#' published in 1895.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/153}
+#' @format A character vector with 15848 elements
+"jude"
+
