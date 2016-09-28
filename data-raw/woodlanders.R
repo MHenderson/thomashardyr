8d6a06c34596227eda86f1bf3318766d835334d6
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-09-28 18:37:05 +0100

    Use gutenbergr for "The Woodlanders" data.

--- data-raw/woodlanders.R
+++ data-raw/woodlanders.R
@@ -1,20 +1,5 @@
-# $ grep -n "THE WOODLANDERS" pg482.txt
-# 19:*** START OF THIS PROJECT GUTENBERG EBOOK THE WOODLANDERS ***
-# 26:THE WOODLANDERS
-# 15038:*** END OF THIS PROJECT GUTENBERG EBOOK THE WOODLANDERS ***
-begin_skip <- 26 - 1
-# $ wc -l pg482.txt 
-# 15396 pg482.txt
-# $ grep -n "END" pg482.txt 
-# 15038:*** END OF THIS PROJECT GUTENBERG EBOOK THE WOODLANDERS ***
-# 15356:SEND DONATIONS or determine the status of compliance for any
-# $ echo "15396 - 15038" | bc
-# 358
-end_skip <- 358 + 3
-book_url <- "http://www.gutenberg.org/cache/epub/482/pg482.txt"
+library(gutenbergr)
 
-woodlanders <- readr::read_lines(book_url, skip = begin_skip)
-woodlanders <- woodlanders[1:(length(woodlanders) - end_skip)]
-woodlanders <- woodlanders[!is.na(woodlanders)]
+woodlanders <- gutenberg_download(482)$text
 
 devtools::use_data(woodlanders, overwrite = TRUE)
